03edf1b33fe1838152c60d7181ea8d9d4ebc6860
Minami Tomonobu <minami@edanoue.com>
2022-12-06 09:43:02 +0900

    Impl v0.1.2 interfaces

--- Runtime/Scripts/OVR/OvrControllerQuestTouch.cs
+++ Runtime/Scripts/OVR/OvrControllerQuestTouch.cs
@@ -12,26 +12,20 @@ namespace Edanoue.VR.Device.Quest
     public class OvrControllerQuestTouch : IController, ISupportedVelocity, IVibration,
         IUpdatable
     {
+        private const float InputTolerance = 0.0001f;
         private readonly ControllerDomain _controllerDomain;
-        private readonly ControllerInputData _controllerInputData;
+        private readonly ControllerInputData _inputCache;
         private OVRPlugin.PoseStatef _cachedPoseState;
-        private Action<float, float>? _changedStickDelegate;
         private Action? _establishedConnectionDelegate;
 
         private bool _isConnected;
 
         private Action? _lostConnectionDelegate;
-        private Action<bool>? _pressedPrimaryDelegate;
-        private Action<bool>? _pressedSecondaryDelegate;
-        private Action<bool>? _pressedStickDelegate;
-        private Action<bool>? _touchedPrimaryDelegate;
-        private Action<bool>? _touchedSecondaryDelegate;
-        private Action<bool>? _touchedStickDelegate;
 
         internal OvrControllerQuestTouch(ControllerDomain controllerDomain)
         {
             _controllerDomain = controllerDomain;
-            _controllerInputData = new ControllerInputData();
+            _inputCache = new ControllerInputData();
         }
 
         private OVRInput.Controller _ovrControllerMask
@@ -48,60 +42,100 @@ namespace Edanoue.VR.Device.Quest
         }
 
         ControllerDomain IController.Domain => _controllerDomain;
-        bool IController.IsPressedPrimary => _controllerInputData.IsPressedPrimary;
-        bool IController.IsTouchedPrimary => _controllerInputData.IsTouchedPrimary;
+        bool IController.IsPressedPrimary => _inputCache.IsPressedPrimary;
+        bool IController.IsTouchedPrimary => _inputCache.IsTouchedPrimary;
 
         event Action<bool>? IController.PressedPrimary
         {
-            add => _pressedPrimaryDelegate += value;
-            remove => _pressedPrimaryDelegate -= value;
+            add => _inputCache.PressedPrimary += value;
+            remove => _inputCache.PressedPrimary -= value;
         }
 
         event Action<bool>? IController.TouchedPrimary
         {
-            add => _touchedPrimaryDelegate += value;
-            remove => _touchedPrimaryDelegate -= value;
+            add => _inputCache.TouchedPrimary += value;
+            remove => _inputCache.TouchedPrimary -= value;
         }
 
-        bool IController.IsPressedSecondary => _controllerInputData.IsPressedSecondary;
-        bool IController.IsTouchedSecondary => _controllerInputData.IsTouchedSecondary;
+        bool IController.IsPressedSecondary => _inputCache.IsPressedSecondary;
+        bool IController.IsTouchedSecondary => _inputCache.IsTouchedSecondary;
 
         event Action<bool>? IController.PressedSecondary
         {
-            add => _pressedSecondaryDelegate += value;
-            remove => _pressedSecondaryDelegate -= value;
+            add => _inputCache.PressedSecondary += value;
+            remove => _inputCache.PressedSecondary -= value;
         }
 
         event Action<bool>? IController.TouchedSecondary
         {
-            add => _touchedSecondaryDelegate += value;
-            remove => _touchedSecondaryDelegate -= value;
+            add => _inputCache.TouchedSecondary += value;
+            remove => _inputCache.TouchedSecondary -= value;
         }
 
-        bool IController.IsPressedStick => _controllerInputData.IsPressedStick;
-        bool IController.IsTouchedStick => _controllerInputData.IsTouchedStick;
+        bool IController.IsTouchedTrigger => _inputCache.IsTouchedTrigger;
+        float IController.Trigger => _inputCache.Trigger;
 
-        (float X, float Y) IController.Stick => _controllerInputData.Stick;
+        event Action<bool>? IController.TouchedTrigger
+        {
+            add => _inputCache.TouchedTrigger += value;
+            remove => _inputCache.TouchedTrigger -= value;
+        }
+
+        event Action<float>? IController.ChangedTrigger
+        {
+            add => _inputCache.ChangedTrigger += value;
+            remove => _inputCache.ChangedTrigger -= value;
+        }
+
+        /// <summary>
+        ///     Quest Touch Controller always return false.
+        /// </summary>
+        bool IController.IsTouchedGrip => _inputCache.IsTouchedGrip;
+
+        float IController.Grip => _inputCache.Grip;
+
+        event Action<bool>? IController.TouchedGrip
+        {
+            add => _inputCache.TouchedGrip += value;
+            remove => _inputCache.TouchedGrip -= value;
+        }
+
+        event Action<float>? IController.ChangedGrip
+        {
+            add => _inputCache.ChangedGrip += value;
+            remove => _inputCache.ChangedGrip -= value;
+        }
+
+        bool IController.IsPressedStick => _inputCache.IsPressedStick;
+        bool IController.IsTouchedStick => _inputCache.IsTouchedStick;
+
+        (float X, float Y) IController.Stick => _inputCache.Stick;
 
         event Action<bool>? IController.PressedStick
         {
-            add => _pressedStickDelegate += value;
-            remove => _pressedPrimaryDelegate -= value;
+            add => _inputCache.PressedStick += value;
+            remove => _inputCache.PressedStick -= value;
         }
 
         event Action<bool>? IController.TouchedStick
         {
-            add => _touchedStickDelegate += value;
-            remove => _touchedStickDelegate -= value;
+            add => _inputCache.TouchedStick += value;
+            remove => _inputCache.TouchedStick -= value;
         }
 
         event Action<float, float>? IController.ChangedStick
         {
-            add => _changedStickDelegate += value;
-            remove => _changedStickDelegate -= value;
+            add => _inputCache.ChangedStick += value;
+            remove => _inputCache.ChangedStick -= value;
         }
 
-        bool IController.IsTouchedThumbRest => _controllerInputData.IsTouchedThumbRest;
+        bool IController.IsTouchedThumbRest => _inputCache.IsTouchedThumbRest;
+
+        event Action<bool> IController.TouchedThumbRest
+        {
+            add => _inputCache.TouchedThumbRest += value;
+            remove => _inputCache.TouchedThumbRest -= value;
+        }
 
         bool ITracker.IsConnected => _isConnected;
 
@@ -229,77 +263,226 @@ namespace Edanoue.VR.Device.Quest
             // --------------------------------------
             // Cache buttons
             // --------------------------------------
-            tmpBool = OVRInput.Get(OVRInput.Button.One, _ovrControllerMask);
-            if (_controllerInputData.IsPressedPrimary != tmpBool)
-            {
-                _controllerInputData.IsPressedPrimary = tmpBool;
-                _pressedPrimaryDelegate?.Invoke(tmpBool);
-            }
+            // primary (A or X) pressed
+            _inputCache.IsPressedPrimary = OVRInput.Get(OVRInput.Button.One, _ovrControllerMask);
 
-            tmpBool = OVRInput.Get(OVRInput.Touch.One, _ovrControllerMask);
-            if (_controllerInputData.IsTouchedPrimary != tmpBool)
-            {
-                _controllerInputData.IsTouchedPrimary = tmpBool;
-                _touchedPrimaryDelegate?.Invoke(tmpBool);
-            }
+            // primary (A or X) touched
+            _inputCache.IsTouchedPrimary = OVRInput.Get(OVRInput.Touch.One, _ovrControllerMask);
 
-            tmpBool = OVRInput.Get(OVRInput.Button.Two, _ovrControllerMask);
-            if (_controllerInputData.IsPressedSecondary != tmpBool)
-            {
-                _controllerInputData.IsPressedSecondary = tmpBool;
-                _pressedSecondaryDelegate?.Invoke(tmpBool);
-            }
+            // secondary button (B or Y) pressed
+            _inputCache.IsPressedSecondary = OVRInput.Get(OVRInput.Button.Two, _ovrControllerMask);
 
-            tmpBool = OVRInput.Get(OVRInput.Touch.Two, _ovrControllerMask);
-            if (_controllerInputData.IsTouchedSecondary != tmpBool)
-            {
-                _controllerInputData.IsTouchedSecondary = tmpBool;
-                _touchedSecondaryDelegate?.Invoke(tmpBool);
-            }
+            // secondary button (B or Y) touched
+            _inputCache.IsTouchedSecondary = OVRInput.Get(OVRInput.Touch.Two, _ovrControllerMask);
 
-            tmpBool = OVRInput.Get(OVRInput.Button.PrimaryThumbstick, _ovrControllerMask);
-            if (_controllerInputData.IsPressedStick != tmpBool)
-            {
-                _controllerInputData.IsPressedStick = tmpBool;
-                _pressedStickDelegate?.Invoke(tmpBool);
-            }
+            // trigger touched
+            _inputCache.IsTouchedTrigger = OVRInput.Get(OVRInput.Touch.PrimaryIndexTrigger, _ovrControllerMask);
 
-            tmpBool = OVRInput.Get(OVRInput.Touch.PrimaryThumbstick, _ovrControllerMask);
-            if (_controllerInputData.IsTouchedStick != tmpBool)
-            {
-                _controllerInputData.IsTouchedStick = tmpBool;
-                _touchedStickDelegate?.Invoke(tmpBool);
-            }
+            // trigger value
+            _inputCache.Trigger = OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, _ovrControllerMask);
 
+            // Note: Oculus Quest Touch controller can't detect grip touch
+
+            // grip value
+            _inputCache.Grip = OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, _ovrControllerMask);
+
+            // thumbstick pressed
+            _inputCache.IsPressedStick = OVRInput.Get(OVRInput.Button.PrimaryThumbstick, _ovrControllerMask);
+
+            // thumbstick touched
+            _inputCache.IsTouchedStick = OVRInput.Get(OVRInput.Touch.PrimaryThumbstick, _ovrControllerMask);
+
+            // thumbstick 2D value
             var tmpVec2 = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick, _ovrControllerMask);
-            if (_controllerInputData.StickX != tmpVec2.x || _controllerInputData.StickY != tmpVec2.y)
-            {
-                _controllerInputData.StickX = tmpVec2.x;
-                _controllerInputData.StickY = tmpVec2.y;
-                _changedStickDelegate?.Invoke(_controllerInputData.StickX, _controllerInputData.StickY);
-            }
+            _inputCache.Stick = (tmpVec2.x, tmpVec2.y);
 
-            tmpBool = OVRInput.Get(OVRInput.Touch.PrimaryThumbRest, _ovrControllerMask);
-            if (_controllerInputData.IsTouchedThumbRest != tmpBool) _controllerInputData.IsTouchedThumbRest = tmpBool;
+            // thumb rest touched
+            _inputCache.IsTouchedThumbRest = OVRInput.Get(OVRInput.Touch.PrimaryThumbRest, _ovrControllerMask);
         }
 
+        /// <summary>
+        /// </summary>
         private class ControllerInputData
         {
-            public float Grip;
-            public bool IsPressedPrimary;
-            public bool IsPressedSecondary;
-            public bool IsPressedStick;
-            public bool IsTouchedGrip;
-            public bool IsTouchedPrimary;
-            public bool IsTouchedSecondary;
-            public bool IsTouchedStick;
-            public bool IsTouchedThumbRest;
-            public bool IsTouchedTrigger;
-            public float StickX;
-            public float StickY;
-            public float Trigger;
+            private float _grip;
+            private bool _isPressedPrimary;
+            private bool _isPressedSecondary;
+            private bool _isPressedStick;
+            private bool _isTouchedGrip;
+            private bool _isTouchedPrimary;
+            private bool _isTouchedSecondary;
+            private bool _isTouchedStick;
+            private bool _isTouchedThumbRest;
+            private bool _isTouchedTrigger;
+            private float _stickX;
+            private float _stickY;
+            private float _trigger;
+            internal Action<float>? ChangedGrip;
+            internal Action<float, float>? ChangedStick;
+            internal Action<float>? ChangedTrigger;
 
-            internal (float X, float Y) Stick => (StickX, StickY);
+            internal Action<bool>? PressedPrimary;
+            internal Action<bool>? PressedSecondary;
+            internal Action<bool>? PressedStick;
+            internal Action<bool>? TouchedGrip;
+            internal Action<bool>? TouchedPrimary;
+            internal Action<bool>? TouchedSecondary;
+            internal Action<bool>? TouchedStick;
+            internal Action<bool>? TouchedThumbRest;
+            internal Action<bool>? TouchedTrigger;
+
+            internal bool IsPressedPrimary
+            {
+                get => _isPressedPrimary;
+                set
+                {
+                    if (value ^ _isPressedPrimary)
+                    {
+                        _isPressedPrimary = value;
+                        PressedPrimary?.Invoke(value);
+                    }
+                }
+            }
+
+            internal bool IsTouchedPrimary
+            {
+                get => _isTouchedPrimary;
+                set
+                {
+                    if (value ^ _isTouchedPrimary)
+                    {
+                        _isTouchedPrimary = value;
+                        TouchedPrimary?.Invoke(value);
+                    }
+                }
+            }
+
+            internal bool IsPressedSecondary
+            {
+                get => _isPressedSecondary;
+                set
+                {
+                    if (value ^ _isPressedSecondary)
+                    {
+                        _isPressedSecondary = value;
+                        PressedSecondary?.Invoke(value);
+                    }
+                }
+            }
+
+            internal bool IsTouchedSecondary
+            {
+                get => _isTouchedSecondary;
+                set
+                {
+                    if (value ^ _isTouchedSecondary)
+                    {
+                        _isTouchedSecondary = value;
+                        TouchedSecondary?.Invoke(value);
+                    }
+                }
+            }
+
+            internal bool IsTouchedTrigger
+            {
+                get => _isTouchedTrigger;
+                set
+                {
+                    if (value ^ _isTouchedTrigger)
+                    {
+                        _isTouchedTrigger = value;
+                        TouchedTrigger?.Invoke(value);
+                    }
+                }
+            }
+
+            internal float Trigger
+            {
+                get => _trigger;
+                set
+                {
+                    if (!(Math.Abs(value - _trigger) > InputTolerance)) return;
+                    _trigger = value;
+                    ChangedTrigger?.Invoke(value);
+                }
+            }
+
+            internal bool IsTouchedGrip
+            {
+                get => _isTouchedGrip;
+                set
+                {
+                    if (value ^ _isTouchedGrip)
+                    {
+                        _isTouchedGrip = value;
+                        TouchedGrip?.Invoke(value);
+                    }
+                }
+            }
+
+            internal float Grip
+            {
+                get => _grip;
+                set
+                {
+                    if (!(Math.Abs(value - _grip) > InputTolerance)) return;
+                    _grip = value;
+                    ChangedGrip?.Invoke(value);
+                }
+            }
+
+            internal bool IsPressedStick
+            {
+                get => _isPressedStick;
+                set
+                {
+                    if (value ^ _isPressedStick)
+                    {
+                        _isPressedStick = value;
+                        PressedStick?.Invoke(value);
+                    }
+                }
+            }
+
+            internal bool IsTouchedStick
+            {
+                get => _isTouchedStick;
+                set
+                {
+                    if (value ^ _isTouchedStick)
+                    {
+                        _isTouchedStick = value;
+                        TouchedStick?.Invoke(value);
+                    }
+                }
+            }
+
+            internal (float X, float Y) Stick
+            {
+                get => (_stickX, _stickY);
+                set
+                {
+                    if (Math.Abs(value.X - _stickX) > InputTolerance || Math.Abs(value.Y - _stickY) > InputTolerance)
+                    {
+                        _stickX = value.X;
+                        _stickY = value.Y;
+                        ChangedStick?.Invoke(value.X, value.Y);
+                    }
+                }
+            }
+
+            internal bool IsTouchedThumbRest
+            {
+                get => _isTouchedThumbRest;
+                set
+                {
+                    if (value ^ _isTouchedThumbRest)
+                    {
+                        _isTouchedThumbRest = value;
+                        TouchedThumbRest?.Invoke(value);
+                    }
+                }
+            }
         }
     }
 }
--- Runtime/Scripts/Types.cs
+++ Runtime/Scripts/Types.cs
@@ -4,10 +4,13 @@ namespace Edanoue.VR.Device.Quest
     internal enum HeadsetType
     {
         Unknown = 0,
+
         // Oculus Quest
         Quest = 1,
+
         // Oculus Quest 2
         Quest_2 = 2,
+
         // Meta Quest Pro
         Quest_Pro = 3
     }
@@ -15,8 +18,10 @@ namespace Edanoue.VR.Device.Quest
     internal enum ControllerType
     {
         Unknown = 0,
+
         // Oculus Quest, Oculus Quest 2 controller
         Quest_Touch = 1,
+
         // Meta Quest Pro controller
         Quest_Touch_Pro = 2
     }
