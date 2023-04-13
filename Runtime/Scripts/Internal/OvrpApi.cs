375b0e296bc4f5d4f68f84022e49fa3aa3858803
Minami Tomonobu <minami@edanoue.com>
2023-04-13 15:10:29 +0900

    :fire: Controller の State(入力) 取る API 追加

--- Runtime/Scripts/Internal/OvrpApi.cs
+++ Runtime/Scripts/Internal/OvrpApi.cs
@@ -10,14 +10,23 @@ namespace Edanoue.VR.Device.Quest.Internal
     /// </summary>
     internal static class OvrpApi
     {
-        private const string PluginName = "OVRPlugin";
+        private const string _PLUGIN_NAME = "OVRPlugin";
 
         /// <summary>
         /// </summary>
         /// <param name="stepId"></param>
         /// <param name="nodeId"></param>
         /// <returns></returns>
-        [DllImport(PluginName, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(_PLUGIN_NAME, CallingConvention = CallingConvention.Cdecl)]
         internal static extern OVRPlugin.PoseStatef ovrp_GetNodePoseState(OVRPlugin.Step stepId, OVRPlugin.Node nodeId);
+
+        /// <summary>
+        /// </summary>
+        /// <param name="controllerMask"></param>
+        /// <param name="controllerState"></param>
+        /// <returns></returns>
+        [DllImport(_PLUGIN_NAME, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern OVRPlugin.Result ovrp_GetControllerState5(uint controllerMask,
+            ref OVRPlugin.ControllerState5 controllerState);
     }
 }
